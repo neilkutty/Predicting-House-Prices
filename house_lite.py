09b0a1648635c19b5b98da8d496a91eda070f0f1
sampsonsimpson <NNK@Neilss-MacBook-Pro.local>
2017-04-20 22:22:40 -0400

    ipynb updates

--- house_lite.py
+++ house_lite.py
@@ -11,7 +11,6 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import plotly
